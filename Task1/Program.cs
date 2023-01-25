71d2933252932f04dca7d4639619144607761496
Darya <iontseva.dasha>
2023-01-26 00:03:06 +0300

    second commit

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int input = Prompt("Введите число > ");
+int integer = input;
+int revers = 0;
+while (integer > 0)
+{
+    revers = revers * 10 + integer % 10;
+    integer = integer / 10;
+}
+if (input == revers)
+{
+    Console.WriteLine($"Число {input} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {input} не является палиндромом");
+}
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int volue = Prompt("Введите число");
+Math.Pow(volue,2);   
+int i = 1;
+while (i <= volue)
+{
+double r =  Math.Pow(i,3);
+Console.WriteLine($"Куб числа {i} от 1 до введенного равен {r}");
+i++;
+} 
+
+
+
